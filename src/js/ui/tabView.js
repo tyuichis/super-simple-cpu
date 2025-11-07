5cc789e9f7d070b80cda07a2e9c1a7110cf05169
tyuichis <tommyyuichisiek@gmail.com>
2025-11-07 14:14:31 -0800

    feat(js:ui): add tab view change handler

---
+++ src/js/ui/tabView.js
@@ -0,0 +1,27 @@
+/* Handles tab view
+
+Clicking on a tab should hide the current view and enable the new view.
+
+*/
+
+function displayTabView(targetButtonElement) {
+  const mainElement = document.querySelector("main");
+  // get just the end of the string
+  // on-main → main
+  const currentViewName = mainElement.className.replace("on-", "");
+
+  // format the targetViewName
+  // "Log Window" → log-window
+  const targetViewName = targetButtonElement.textContent.trim().replaceAll(" ", "-").toLowerCase();
+
+    console.log(targetViewName);
+
+  // toggle and set correct class.
+  if (currentViewName !== targetViewName) {
+    // only run this when they're not the same to prevent flickering
+    mainElement.classList.toggle(`on-${currentViewName}`);
+    mainElement.classList.toggle(`on-${targetViewName}`);
+  }
+}
+
+export { displayTabView };
