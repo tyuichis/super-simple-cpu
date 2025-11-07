8b3af9fe0cf76cf7823bca27ffd35782dfe4aa84
tyuichis <tommyyuichisiek@gmail.com>
2025-11-07 14:13:57 -0800

    feat(js:utils): add clear functions for memory, io, registers

--- src/js/utils/clearApp.js
+++ src/js/utils/clearApp.js
@@ -1,28 +1,39 @@
 /* Clear every slot in the memory to its default
  */
 
-function clearApp() {
-  // Clear i/o
+import { MEMORY_SIZE } from "./constants.js";
+
+function clearIO() {
   const ioInput = document.getElementById("io-input");
   const ioOutput = document.getElementById("io-output");
   ioInput.value = "";
   ioOutput.value = "";
+}
 
-  // Clear CPU registers
-  document.querySelectorAll("#cpu input").forEach((input) => {
-    input.value = "";
-  });
-
-  // Clear memory
-  for (let i = 0; i <= 15; i++) {
+function clearMemory() {
+  for (let i = 0; i <= MEMORY_SIZE - 1; i++) {
     const memAddrLabel = document.getElementById(`mem-addr-label-${i}`);
     const memAddr = document.getElementById(`mem-addr-${i}`);
     const memVal = document.getElementById(`mem-val-${i}`);
+    const memValInstruction = document.getElementById(`mem-val-${i}-asm`);
 
     memAddrLabel.value = "";
     memAddr.value = "";
     memVal.value = "";
+    memValInstruction.textContent = "";
   }
 }
 
-export { clearApp };
+function clearCPURegisters() {
+  document.querySelectorAll("#cpu input").forEach((input) => {
+    input.value = "";
+  });
+}
+
+function clearApp() {
+  clearIO();
+  clearMemory();
+  clearCPURegisters();
+}
+
+export { clearIO, clearMemory, clearCPURegisters, clearApp };
