6565f0a98238b653b5db05e2c092cfe9785efc92
tyuichis <tommyyuichisiek@gmail.com>
2025-11-07 14:17:20 -0800

    feat(js:ui): add display example selection handler

---
+++ src/js/ui/loadExample.js
@@ -0,0 +1,20 @@
+/* Loads examples from the textbook, and possibly new ones.
+
+// Add new examples within exampleList.js (for now).
+
+*/
+
+// Populate RAM cells with examples.
+
+function displayExample(example) {
+  // Only display the elements we need;
+  for (let i = 0; i < example.length; i++) {
+    console.log(example);
+    const memoryVal = document.getElementById(`mem-val-${i}`);
+    const memoryLabel = document.getElementById(`mem-addr-label-${i}`);
+    memoryVal.value = example[i]["mem-val"];
+    memoryLabel.textContent = example[i]["mem-label"];
+  }
+}
+
+export { displayExample };
