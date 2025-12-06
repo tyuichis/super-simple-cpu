0f1099af0f06643f33290a6e0f466b49fa700c3b
tyuichis <tommyyuichisiek@gmail.com>
2025-12-06 00:53:27 -0800

    refactor(js): add comments; may be deprecated

--- src/js/ui/inputDisplay.js
+++ src/js/ui/inputDisplay.js
@@ -1,8 +1,12 @@
 /* Helper functions that get, sanitize and modify the input behavior
  */
 
+import {
+  convertBinaryToDecimal,
+} from "../utils/binary.js";
+
 // Intended usage:
-// getRawInput → preventNonBinaryDigits → addBinaryPadding
+// getRawInput → preventNonBinaryDigits → addBinaryPadding (from binary.js)
 
 function getRawInput(inputString) {
   return inputString.replaceAll(" ", "");
@@ -24,22 +28,78 @@ function preventNonBinaryDigits(rawInput) {
   return binaryDigits;
 }
 
-function addBinaryPadding(binaryRawInput) {
-  /* Add a space every 4 bits for readability
-   */
+function padBinaryToFormat(inputString, size) {
+  /* Runs when the user presses start or run but has incomplete digits
+  i.e. 0011 0000 0000 01
+                        ^^
+                        Missing 2 bits
+    format to → 0011 0000 0000 0100
 
-  let binaryPaddedString = "";
+    and 0011 001 → 0011 0010 0000 0000
+  */
 
-  for (let i = 0; i < binaryRawInput.length; i++) {
-    // Don't include a space in the beginning or end.
-    if (i !== 0 && i % 4 === 0 && i !== binaryRawInput.length - 1) {
-      binaryPaddedString += ` ${binaryRawInput[i]}`;
-    } else {
-      binaryPaddedString += binaryRawInput[i];
-    }
-  }
-
-  return binaryPaddedString;
+  // sanitize value first so we can build our string from scratch.
+  let paddedBinary = getRawInput(inputString);
+  paddedBinary = paddedBinary.padEnd(size, "0");
+  return formatBinaryWithPadding(paddedBinary);
 }
 
-export { getRawInput, preventNonBinaryDigits, addBinaryPadding };
+
+// need to refactor to just accept instruction, binary
+function displayASMValue(rawBinaryValue, requireOperand = true, addressMode = false) {
+  /*
+  Show unsigned and two's complement version.
+
+  ALSO, control whether the operand is shown i.e., if the OPcode doesn't require it.
+
+  I don't want to refactor classes, so I'll just declare keyword arguments to simplify my life;
+  
+  if we have 'addressMode', then, return just the unsigned value (no negative number-- doesn't matter)
+  if we have requireOperand, show the operand. else set it to false manually (i.e. for JMP.)
+  */
+
+  if (!requireOperand) {
+    return "";
+  }
+
+  const unsigned = parseInt(rawBinaryValue, 2);
+  const signed = parseInt(convertBinaryToDecimal(rawBinaryValue), 10);
+
+  // console.log(`signed: ${signed}`);
+  // check if it was originally a negative value AND not in address mode
+  if (signed < 0 && !addressMode) {
+    return `${unsigned} (${signed})`;
+  } else {
+    // console.log(`addressMode: ${addressMode}`)
+    // return `${unsigned}`;
+  }
+}
+
+// TO-DO: make a function that keeps track of the input keyboard cursor
+// because we change the input value on change, and the browser
+// doesn't notice
+
+
+function formatBinaryWithPadding(rawInput) {
+  /* Adds a space every 4 digits (max: 3 spaces for a 16 bit number)
+   */
+  let formattedString = "";
+
+  for (let i = 0; i < rawInput.length; i++) {
+    if (i > 0 && i % 4 === 0) {
+      formattedString += " ";
+    }
+    formattedString += rawInput[i];
+  }
+
+  return formattedString;
+}
+
+
+export {
+  getRawInput,
+  preventNonBinaryDigits,
+  padBinaryToFormat,
+  displayASMValue,
+  formatBinaryWithPadding
+};
